79e0aacb3ea43a64797f0fce44ffeb06dccf5172
Bastian Ceppi <bceppi@ordename.cl>
2023-03-01 11:55:20 -0300

    fix: removed empty tags

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -15,49 +15,51 @@ const Auth = ({ title, handleAuthSubmit }) => {
   };
 
   return (
-    <div className="container">
-      <div className="columns">
-        <div className="column is-half is-offset-one-quarter">
-          <h1 className="title">{title}</h1>
-          <div className="form-container is-white">
-            <form onSubmit={handleSubmit}>
-              <div className="field">
-                <label className="label">Email</label>
-                <input
-                  className="input"
-                  name="email"
-                  label="Email"
-                  type="email"
-                  value={email}
-                  onChange={handleChange}
-                  autoComplete="work-email"
-                />
-              </div>
-              <div className="field">
-                <label className="label">Contraseña</label>
-                <input
-                  className="input"
-                  name="password"
-                  label="Contraseña"
-                  type="password"
-                  value={password}
-                  onChange={handleChange}
-                  autoComplete="new-password"
-                />
-              </div>
-              <div className="field">
-                <button className="button is-primary mb-1" type="submit">
-                  Iniciar sesión
-                </button>
-                <p className="help">
-                  <a href="#">Olvidé mi contraseña</a>
-                </p>
-              </div>
-            </form>
+    <>
+      <div className="container">
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            <h1 className="title">{title}</h1>
+            <div className="form-container is-white">
+              <form onSubmit={handleSubmit}>
+                <div className="field">
+                  <label className="label">Email</label>
+                  <input
+                    className="input"
+                    name="email"
+                    label="Email"
+                    type="email"
+                    value={email}
+                    onChange={handleChange}
+                    autoComplete="work-email"
+                  />
+                </div>
+                <div className="field">
+                  <label className="label">Contraseña</label>
+                  <input
+                    className="input"
+                    name="password"
+                    label="Contraseña"
+                    type="password"
+                    value={password}
+                    onChange={handleChange}
+                    autoComplete="new-password"
+                  />
+                </div>
+                <div className="field">
+                  <button className="button is-primary mb-1" type="submit">
+                    Iniciar sesión
+                  </button>
+                  <p className="help">
+                    <a href="#">Olvidé mi contraseña</a>
+                  </p>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
